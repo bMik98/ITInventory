631b4b7ccb1c0f8f5a05f532339f997beb12a363
bMik98 <bmik9898@gmail.com>
2016-12-19 18:49:38 +0300

    refactoring finished

--- LogImport/src/main/java/ru/regionsgroup/inventory/Main.java
+++ LogImport/src/main/java/ru/regionsgroup/inventory/Main.java
@@ -18,33 +18,6 @@ public class Main {
     }
 
     public void doIt() {
-//        String printerConnectionsLogRoot = "\\\\netapp2.regions.local\\Distibutive\\_Logs\\Log-Printers";
-//        String activeDirectoryLogRoot = "\\\\netapp2.regions.local\\Distibutive\\_Logs\\Log-AD\\";
-//
-//        UserAuditFileSource userAuditDirectory = new UserAuditFileSource(
-//                Paths.get(activeDirectoryLogRoot), "ad_users.json");
-//        ComputerAuditFileSource computerAuditDirectory = new ComputerAuditFileSource(
-//                Paths.get(activeDirectoryLogRoot), "ad_computers");
-//        DomainAuditFileSource domainAuditDirectory = new DomainAuditFileSource(
-//                Paths.get(activeDirectoryLogRoot), "ad_domains.json");
-//        PrinterConnectionAuditFileSource printerConnectionAuditDirectory = new PrinterConnectionAuditFileSource(
-//                Paths.get(printerConnectionsLogRoot), "*.json");
-//
-//        AuditImport usersLoader = new UserAuditSimpleImport(
-//                new HibernateUserDao(), new UserAuditConverter(), userAuditDirectory);
-//        AuditImport computersLoader = new ComputerAuditBasicImport(
-//                new HibernateComputerDao(),
-//                new ComputerAuditConverter(),
-//                computerAuditDirectory);
-//        AuditImport domainsLoader = new DomainAuditBasicImport(
-//                new HibernateDomainDao(),
-//                new DomainAuditConverter(),
-//                domainAuditDirectory);
-//        AuditImport printerConnectionsLoader = new PrinterConnectionAuditSimpleImport(
-//                new HibernatePrinterConnectionDao(),
-//                new PrinterConnectionAuditConverter(),
-//                printerConnectionAuditDirectory);
-
         System.out.println("go go go");
         ApplicationContext context = new AnnotationConfigApplicationContext(AuditSourceConfig.class);
         PrinterConnectionDao prnDao = context.getBean(PrinterConnectionDao.class);
